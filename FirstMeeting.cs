81f4b90cc4fee391c9c201d876eedac9bf876847
MindHardt <71509337+MindHardt@users.noreply.github.com>
2022-09-03 13:36:50 +0500

    Добавьте файлы проекта.

---
+++ FirstMeeting.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace StudentTask
+{
+    public static class FirstMeeting
+    {
+        public static string Loop(string input)
+        {
+            StringBuilder sb = new(input.Length / 2);
+
+            for (int i = 0; i < input.Length; i += 2)
+            {
+                sb.Append(input[i]);
+            }
+
+            return sb.ToString();
+        }
+
+        public static string Linq(string input)
+        {
+            int i = 0;
+            return new string(input.Where(c => (i++ % 2) == 0).ToArray());
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+
+namespace StudentTask
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string input = "Lorem ipsum dolor sit amet";
+            Console.WriteLine(FirstMeeting.Loop(input));
+            Console.WriteLine(FirstMeeting.Linq(input));
+        }
+    }
+}
